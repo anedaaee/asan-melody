1e8fd3d4244711e9afd21bd51016b195bd0a2409
neso <anedaaee@gmail.com>
2023-08-26 17:50:07 +0330

    add organ router and getOrganById method

--- backend/src/controller/organCtrl.js
+++ backend/src/controller/organCtrl.js
@@ -2,13 +2,23 @@ const request = require('../db/request')
 const responseMessage = require('../functions/readMessage')
 const CustomError = require('../functions/customErr')
 
-exports.getOrgan = async (req)=> {
+exports.getOrgans = async (req)=> {
     try{
         const query = `SELECT * FROM organizations;`
 
         const result = await request(query,[],req)
 
         return result;
+
+    }catch(err){throw err}
+}
+exports.getOrganById = async (req , values)=> {
+    try{
+        const query = `SELECT * FROM organizations WHERE id = ?;`
+
+        const result = await request(query,[values.id],req)
+
+        return result[0];
 
     }catch(err){throw err}
 }
--- backend/src/routers/organ.js
+++ backend/src/routers/organ.js
@@ -6,7 +6,7 @@ const router = new express.Router()
 
 router.get('/getOrgans' , async (req,res) => {
     try{
-        const result = await organCtrl.getOrgan(req);
+        const result = await organCtrl.getOrgans(req);
 
         res.status(200).send({
             "metadata": responseMessage(5),
@@ -26,5 +26,34 @@ router.get('/getOrgans' , async (req,res) => {
         })
     }
 })
+router.get('/getOrganById' , async (req,res) => {
+    try{
+        const schema = Joi.object({
+            id : Joi.number()
+                .integer()
+                .required()
+        })
 
+        const values = await schema.validateAsync(req.query)
+
+        const result = await organCtrl.getOrganById(req,values);
+
+        res.status(200).send({
+            "metadata": responseMessage(14),
+            "body": {
+                "type": "object",
+                "data": result
+            }
+        })
+
+    }catch(err){
+        let message = responseMessage(4)
+        if(err.isCustom){
+            message = err.reason
+        }
+        return res.status(400).send({
+            "metadata": message
+        })
+    }
+})
 module.exports = router
