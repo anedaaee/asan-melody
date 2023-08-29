4d378adbad39c9a3159b3e6053eefc6f879c2028
neso <anedaaee@gmail.com>
2023-08-29 14:47:11 +0330

    add reserve

--- backend/src/controller/purchaseCtrl.js
+++ backend/src/controller/purchaseCtrl.js
@@ -4,3 +4,20 @@ const CustomError = require('../functions/customErr')
 const { v4:uuidv4} =  require('uuid')
 const path = require('path');
 const fs = require('fs');
+
+exports.reserve = async(req,values) => {
+    try{
+        const timestamp = new Date().getTime();
+
+        let query = 'INSERT INTO asan_melody.reserved_class (`user`, class, `Date`) VALUES(?, ?, ?);'
+
+        await request(query,[values.user, values.class , timestamp],req)
+        
+        query = 'SELECT * FROM asan_melody.reserved_class WHERE class = ? AND `user` = ?;'
+
+        const result = await request(query,[ values.class , values.user],req)
+        
+        return result[0]
+        
+    }catch(err){throw err}
+}
--- backend/src/routers/purchase.js
+++ backend/src/routers/purchase.js
@@ -4,4 +4,37 @@ const responseMessage = require('../functions/readMessage')
 const purchaseCtrl = require('../controller/purchaseCtrl')
 const router = new express.Router()
 
+router.post('/reserve',async(req,res)=>{
+    try{
+        const schema = Joi.object({
+            user : Joi.string()
+                .required(),
+            class : Joi.string()
+                .required()
+        })
+
+        const values = await schema.validateAsync(req.body)
+
+        const result = await purchaseCtrl.reserve(req,values)
+
+        res.status(200).send({
+            "metadata": responseMessage(5),
+            "body": {
+                "type": "object",
+                "data": result
+            }
+        })
+    }catch(err){
+        let message = responseMessage(4)
+        if(err.details) {
+            if(err.details[0].path[0] === 'id') { message = responseMessage(14)}
+        }
+        if(err.isCustom){
+            message = err.reason
+        }
+        return res.status(400).send({
+            "metadata": message
+        })
+    }
+})
 module.exports = router
